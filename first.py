1a44fca6aec701fc062ccf88a1e762e1a7a39d64
Spinoguy2122 <rigelanthonypalanca@gmail.com>
2025-11-27 18:28:35 +0800

    Add files via upload

---
+++ first.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration=0
+    print("The number entered by the user is", n)
+    iteration+=1
+    print("Total iterations done by code is",iteration,"\n")
+
+printnumber(10)
+printnumber(20)
+print("\n With any 'n' time taken by our code wont change")
